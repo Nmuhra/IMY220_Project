a5a1b075b95f8e784e539db2805514a6bd429c8b
Nabegh <nabegh.muhra25@gmail.com>
2024-09-20 10:55:28 +0200

    .

--- frontend/src/pages/Splash.js
+++ frontend/src/pages/Splash.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/Splash.css';
 
-const Splash = () => {
-  return (
-    <div className="splash-container">
-        <a href='/login'><button className="get-started-btn">GET STARTED</button></a>
-    </div>
-  );
+class Splash extends React.Component {
+    render() {
+        return (
+            <div className="splash-container">
+                <a href='/login'><button className="get-started-btn">GET STARTED</button></a>
+            </div>
+          );
+    }
 };
 
 export default Splash;
